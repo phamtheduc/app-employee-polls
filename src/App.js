b9c354d0a3b33396a8519849ace5b1a1334a4adb
Duc <phamtheduc2k@gmail.com>
2024-04-05 22:42:46 +0700

    implement project employee polls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import 'primeicons/primeicons.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import { Button } from 'primereact/button';
+import { InputText } from "primereact/inputtext";
+import { Route, Routes, BrowserRouter as Router, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import Navigation from './components/Naviagation';
+import { Login } from './components/Login';
+import { LeaderBoard } from './components/LeaderBoard';
+import { AnswerQuestion } from './components/AnswerQuestion';
+import { Home } from './components/Home';
+import { CreateQuestion } from './components/CreateQuestion';
+import { PageNotFound } from './components/PageNotFound';
+import { useSelector, useDispatch } from 'react-redux';
+import { getQuestions } from './slices/question';
 
 function App() {
+  const location = useLocation();
+  const currentUser = useSelector(state => (state.currentUser.value));
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentUser?.length && <Navigation />}
+      <Routes>
+        {currentUser?.length && currentUser[1]?.id?.length ? <>
+          <Route path='/' exact element={<Login />} />
+          <Route path='/home' exact element={<Home />} />
+          <Route path='/leaderboard' exact element={<LeaderBoard />} />
+          <Route path='/add' exact element={<CreateQuestion />} />
+          <Route path='/questions/:question_id' exact element={<AnswerQuestion />} />
+        </>
+        : <Route path='*' exact element={<Login />}/>
+        }
+        <Route path='/404' element={<PageNotFound />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
     </div>
   );
 }
